1c5a0c93f42f0406ee12a4b62a713123a60ef3e1
Saul Fimbres <sfimbres@necodex.com>
2022-11-09 14:42:05 -0700

    asd

--- src/components/CreateTable.jsx
+++ src/components/CreateTable.jsx
@@ -133,140 +133,154 @@ export const CreateTable = () => {
 
   return (
     <>
-      <div className='grid content-center gap-4 auto-rows-auto grid-cols-1 max-w-full'>
-        <div className='rounded-lg bg-gray-100 text-gray-900'>
-          <main className='px-5 pt-5 pb-3'>
-            <h1 className='pb-3 text-xl font-semibold'>📌 Catalogo de practicantes</h1>
+      <div className='px-8 py-32'>
+        <div className='grid gap-8 items-start justify-center'>
+          <div className='relative group'>
+            <div className='absolute -inset-0.5 bg-gradient-to-r from-stone-600 to-gray-300/60 rounded-lg blur opacity-90 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt'></div>
+            <div className='relative p-1 bg-black/20 rounded-lg leading-none flex items-center divide-x divide-gray-600'>
+              <div className='grid content-center gap-4 auto-rows-auto grid-cols-1 max-w-full'>
+                <div className='rounded-lg bg-gray-100 text-gray-900'>
+                  <main className='px-5 pt-5 pb-3'>
+                    <h1 className='pb-3 text-xl font-semibold'>📌 Catalogo de practicantes</h1>
 
-            {/* SearchBar */}
-            <div className='flex justify-between gap-x-2'>
-              <GlobalFilter
-                globalFilter={state.globalFilter}
-                setGlobalFilter={setGlobalFilter}
-                total={total}
-              />
+                    {/* SearchBar */}
+                    <div className='flex justify-between gap-x-2'>
+                      <GlobalFilter
+                        globalFilter={state.globalFilter}
+                        setGlobalFilter={setGlobalFilter}
+                        total={total}
+                      />
 
-              {/* Boton Crear practicante */}
-              <div
-                className='cursor-pointer w-fit'
-                title='Agregar Practicante'
-                onClick={() => {
-                  navigate('/agregar');
-                }}
-              >
-                <button className=' bg-[#1b7772] hover:bg-[#1E847F]/80 text-white font-mono py-2 px-4 border-b-4 border-teal-800 hover:border-teal-900 rounded-full'>
-                  +
-                </button>
-              </div>
-            </div>
-
-            {/* Columns & Rows */}
-            <div className='mt-2 flex flex-col'>
-              <div className='-my-2 overflow-x-auto sm:-mx-auto lg:-mx-0'>
-                <div className='py-2 align-middle inline-block min-w-full sm:px-0 lg:px-8'>
-                  <div className='shadow overflow-hidden border-b border-gray-300 sm:rounded-lg'>
-                    <table {...getTableProps()} className='min-w-full divide-y divide-gray-300'>
-                      <thead className=' bg-gray-50'>
-                        {headerGroups.map((headerGroup) => (
-                          <tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map((column) => (
-                              <th
-                                scope='col'
-                                className='px-10 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
-                                {...column.getHeaderProps()}
-                              >
-                                {column.render('Header')}
-                              </th>
-                            ))}
-                          </tr>
-                        ))}
-                      </thead>
-
-                      <tbody
-                        {...getTableBodyProps()}
-                        className='bg-gray-50 divide-y divide-gray-200'
+                      {/* Boton Crear practicante */}
+                      <div
+                        className='cursor-pointer w-fit'
+                        title='Agregar Practicante'
+                        onClick={() => {
+                          navigate('/agregar');
+                        }}
                       >
-                        {page.map((row, i) => {
-                          prepareRow(row);
-                          return (
-                            <tr {...row.getRowProps()}>
-                              {row.cells.map((cell) => {
-                                return (
-                                  <td
-                                    {...cell.getCellProps()}
-                                    className='px-6 py-5 whitespace-nowrap'
-                                  >
-                                    {cell.render('Cell')}
-                                  </td>
-                                );
-                              })}
-                            </tr>
-                          );
-                        })}
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-              </div>
-            </div>
+                        <button className='bg-[#1b7772] hover:bg-[#1E847F]/80 text-white font-mono py-2 px-4 border-teal-800 hover:border-teal-900 rounded-full'>
+                          Agregar
+                        </button>
+                      </div>
+                    </div>
 
-            {/* Pagination */}
-            <div className='py-3 flex items-center justify-between'>
-              <div className='flex-1 flex items-center justify-between'>
-                <div className='flex gap-x-2'>
-                  <span className='text-sm text-gray-700'>
-                    Pagina <span className='font-medium'>{actualPage}</span> de
-                    <span className='pl-1 font-medium'>{totalPages}</span>
-                  </span>
-                  <label>
-                    <span className='sr-only'>Items Per Page</span>
-                    <select
-                      className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
-                      value={limit}
-                      onChange={(e) => {
-                        setLimit(Number(e.target.value));
-                      }}
-                    >
-                      {[5, 10, 20].map((pageSize) => (
-                        <option key={pageSize} value={pageSize}>
-                          Mostrar {pageSize}
-                        </option>
-                      ))}
-                    </select>
-                  </label>
-                </div>
-                <div>
-                  <nav
-                    className='relative z-0 inline-flex rounded-md shadow-sm -space-x-px'
-                    aria-label='Pagination'
-                  >
-                    {actualPage <= 1 ? (
-                      <PageButton onClick={newpreviousPage} disabled>
-                        <span className='sr-only'>Previous</span>
-                        <ChevronLeftIcon className='h-5 w-5' aria-hidden='true' />
-                      </PageButton>
-                    ) : (
-                      <PageButton onClick={newpreviousPage}>
-                        <span className='sr-only'>Previous</span>
-                        <ChevronLeftIcon className='h-5 w-5' aria-hidden='true' />
-                      </PageButton>
-                    )}
-                    {actualPage >= totalPages ? (
-                      <PageButton onClick={newnextPage} disabled>
-                        <span className='sr-only'>Next</span>
-                        <ChevronRightIcon className='h-5 w-5' aria-hidden='true' />
-                      </PageButton>
-                    ) : (
-                      <PageButton onClick={newnextPage}>
-                        <span className='sr-only'>Next</span>
-                        <ChevronRightIcon className='h-5 w-5' aria-hidden='true' />
-                      </PageButton>
-                    )}
-                  </nav>
+                    {/* Columns & Rows */}
+                    <div className='mt-2 flex flex-col'>
+                      <div className='-my-2 overflow-x-auto sm:-mx-auto lg:-mx-0'>
+                        <div className='py-2 align-middle inline-block min-w-full sm:px-0 lg:px-8'>
+                          <div className='shadow overflow-hidden border-b border-gray-300 sm:rounded-lg'>
+                            <table
+                              {...getTableProps()}
+                              className='min-w-full divide-y divide-gray-300'
+                            >
+                              <thead className=' bg-gray-50'>
+                                {headerGroups.map((headerGroup) => (
+                                  <tr {...headerGroup.getHeaderGroupProps()}>
+                                    {headerGroup.headers.map((column) => (
+                                      <th
+                                        scope='col'
+                                        className='px-10 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+                                        {...column.getHeaderProps()}
+                                      >
+                                        {column.render('Header')}
+                                      </th>
+                                    ))}
+                                  </tr>
+                                ))}
+                              </thead>
+
+                              <tbody
+                                {...getTableBodyProps()}
+                                className='bg-gray-50 divide-y divide-gray-200'
+                              >
+                                {page.map((row) => {
+                                  prepareRow(row);
+                                  return (
+                                    <tr {...row.getRowProps()}>
+                                      {row.cells.map((cell) => {
+                                        return (
+                                          <td
+                                            {...cell.getCellProps()}
+                                            className='px-6 py-5 whitespace-nowrap'
+                                          >
+                                            {cell.render('Cell')}
+                                          </td>
+                                        );
+                                      })}
+                                    </tr>
+                                  );
+                                })}
+                              </tbody>
+                            </table>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    {/* Pagination */}
+                    <div className='py-3 flex items-center justify-between'>
+                      <div className='flex-1 flex items-center justify-between'>
+                        <div className='flex gap-x-2'>
+                          <span className='text-sm text-gray-700'>
+                            Pagina <span className='font-medium'>{actualPage}</span> de
+                            <span className='pl-1 font-medium'>{totalPages}</span>
+                          </span>
+                          <label>
+                            <span className='sr-only'>Items Per Page</span>
+                            <select
+                              className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
+                              value={limit}
+                              onChange={(e) => {
+                                setLimit(Number(e.target.value));
+                                setPage(1);
+                                actualPage <= 1 ? null : setSkip(skip - limit);
+                              }}
+                            >
+                              {[5, 10, 20].map((pageSize) => (
+                                <option key={pageSize} value={pageSize}>
+                                  Mostrar {pageSize}
+                                </option>
+                              ))}
+                            </select>
+                          </label>
+                        </div>
+                        <div>
+                          <nav
+                            className='relative z-0 inline-flex rounded-md shadow-sm -space-x-px'
+                            aria-label='Pagination'
+                          >
+                            {actualPage <= 1 ? (
+                              <PageButton onClick={newpreviousPage} disabled>
+                                <span className='sr-only'>Previous</span>
+                                <ChevronLeftIcon className='h-5 w-5' aria-hidden='true' />
+                              </PageButton>
+                            ) : (
+                              <PageButton onClick={newpreviousPage}>
+                                <span className='sr-only'>Previous</span>
+                                <ChevronLeftIcon className='h-5 w-5' aria-hidden='true' />
+                              </PageButton>
+                            )}
+                            {actualPage >= totalPages ? (
+                              <PageButton onClick={newnextPage} disabled>
+                                <span className='sr-only'>Next</span>
+                                <ChevronRightIcon className='h-5 w-5' aria-hidden='true' />
+                              </PageButton>
+                            ) : (
+                              <PageButton onClick={newnextPage}>
+                                <span className='sr-only'>Next</span>
+                                <ChevronRightIcon className='h-5 w-5' aria-hidden='true' />
+                              </PageButton>
+                            )}
+                          </nav>
+                        </div>
+                      </div>
+                    </div>
+                  </main>
                 </div>
               </div>
             </div>
-          </main>
+          </div>
         </div>
       </div>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import { CreateTable } from '../components/CreateTable';
+import neco from '../resources/neco.png';
 
 export const Home = () => {
   return (
     <>
       {/* Contenedor de todo lo que despliega la pagina */}
-
       <div className='flex flex-col h-screen'>
         {/* Header de la pagina */}
-
-        <div className='p-2 text-center font-mono text-2xl text-gray-300  bg-teal-300/30 md:text-5xl'>
-          Necodex - Catalogo de practicantes
+        <div className='flex fixed w-full border-b-2 border-white/20 p-4 justify-center items-center z-50 bg-black/50 backdrop-blur-sm'>
+          <img src={neco} width='60' className='pr-4' />
+          <span className='font-mono drop-shadow-md text-2xl text-gray-200 md:text-3xl lg:text-4xl'>
+            Necodex - Catalogo de practicantes
+          </span>
         </div>
 
         {/* Mostrar tabla */}
-
         <div className='flex m-auto items-center p-2'>
           <CreateTable />
         </div>
 
         {/* Footer de la pagina */}
-
         <div className='mb-2 p-4 items-center text-gray-500/40 text-center text-sm'>
           {'© 2022 Saul Fimbres. All Rights Reserved.'}
         </div>
